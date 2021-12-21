9b05c30222ff2170ca69b42da82be8500f241c1f
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-21 18:39:06 -0500

    Add a Dockerfile to build an image

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,8 @@ import { Container, Row, Col} from "react-bootstrap"
 import React, { useState} from "react"
 import calculator from "../utils/calculator"
 import { ToolTipLite } from 'tooltip-lite';
+import ReactTooltip from 'react-tooltip';
+
 
 
 
