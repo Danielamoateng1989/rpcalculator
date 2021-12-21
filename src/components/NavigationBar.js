2637cde239b7790fb38ae5a153f678f29e6a4a68
Daniel Amoateng <danielamoateng81@yahoo.com>
2021-12-21 00:49:14 -0500

    Install a popup

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,13 +1,14 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const Header = () => {
 
 return (   
  <nav className='nav'>
       <div className="nav-center">
         <div className='nav-header'>
-        <a class="active" href="/">Home</a>
-        <a href="/search"> Search</a>
-        <a href="/contact"> Contact</a>
-        <a href="/login"> Login</a>
+        <a class="active" href="#">Home</a>
+        <a href="#"> Search</a>
+        <a href="#"> Contact</a>
+        <a href="#"> Login</a>
         </div>
      </div>
     </nav>
